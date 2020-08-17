cdf75d839ede4b169604d63ffe8678b893011995
Alfonso <alfonso1011@gmail.com>
2020-08-16 20:48:42 -0500

    Call to the api movieDB

--- movies/app/src/main/java/com/example/movies/MainActivity.java
+++ movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+
+import com.example.movies.models.Action;
+import com.example.movies.models.ResponseMovies;
+import com.example.movies.models.SummaryMovie;
+import com.example.movies.utils.NetworkUtils;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new FetchMovies().execute(Action.POPULAR);
     }
 
     @Override
@@ -19,5 +32,24 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.settings,menu);
         return true;
+    }
+
+    public class FetchMovies extends AsyncTask<Action,Void, List<SummaryMovie>> {
+
+        @Override
+        protected List<SummaryMovie> doInBackground(Action... actions) {
+            if(actions.length == 0)
+                return  null;
+            URL moviesRequested = NetworkUtils.buildURL(actions[0]);
+            Gson gson = new Gson();
+            try {
+                String jsonMovies = NetworkUtils.getResponseFromHttpUrl(moviesRequested);
+                ResponseMovies responseMovies = gson.fromJson(jsonMovies, ResponseMovies.class);
+                return responseMovies.getResults();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
---
+++ movies/app/src/main/java/com/example/movies/utils/NetworkUtils.java
@@ -0,0 +1,85 @@
+package com.example.movies.utils;
+
+import android.net.Uri;
+
+import com.example.movies.models.Action;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public final class NetworkUtils {
+
+    private static class Page {
+        private Action last_action;
+        private int max_pages;
+        private int current_page;
+    }
+
+    private final static String BASE_PATH = "https://api.themoviedb.org/3/movie/";
+    private final static String POPULAR = "popular";
+    private final static String RATED = "top_rated";
+    private static Page current_page = new Page();
+    private final static String API_KEY = "";
+
+
+    public static URL buildURL(Action action) {
+        String uri_path = BASE_PATH.concat(getAction(action));
+        Uri uri = Uri.parse(uri_path).buildUpon().
+                appendQueryParameter("api_key",API_KEY).build();
+        return getUrl(uri);
+    }
+
+    private static URL getUrl(Uri uri) {
+        URL url = null;
+        try {
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static URL buildUrlSearch(String id) {
+        String uri_path = BASE_PATH.concat(id);
+        Uri uri = Uri.parse(uri_path).buildUpon().
+                appendQueryParameter("api_key",API_KEY).build();
+        return getUrl(uri);
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    private static String getAction(Action action) {
+        switch (action) {
+            case RATED:
+                return RATED;
+            default:
+                return POPULAR;
+        }
+    }
+}
